a1a614a5c074f353673278afa2c1ff6a766b58d6
rafa_montesinos <rafael.montesinos@nunsys.com>
2021-05-18 12:25:01 +0200

    1402 - Selección columnas visibles y orden 1403 - Filtros por nombre y por unidad

---
+++ site/src/main/webapp/app/entities/skill-category/list/skill-category.filter.ts
@@ -0,0 +1,20 @@
+import { IUnit } from 'app/entities/unit/unit.model';
+
+export class SkillCategoryFilter {
+  constructor(public name?: string, public unit?: IUnit) {}
+
+  myMap(): any {
+    const map = new Map();
+
+    // NAME
+    if (this.name != null && this.name !== '') {
+      map.set('name.contains', this.name);
+    }
+
+    // UNIT
+    if (this.unit != null && this.unit !== '') {
+      map.set('unitId.equals', this.unit);
+    }
+    return map;
+  }
+}
